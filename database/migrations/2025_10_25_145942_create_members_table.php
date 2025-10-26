969f43983fd0a0928efd6241f95a08869b6025ec
Rifqi Banu Safingi <rifqibanusafingi11@gmail.com>
2025-10-26 10:58:32 +0700

    fix: I accidentally deleted the member migration schema

--- database/migrations/2025_10_25_145942_create_members_table.php
+++ database/migrations/2025_10_25_145942_create_members_table.php
@@ -11,7 +11,21 @@ return new class extends Migration
      */
     public function up(): void
     {
-
+        Schema::create('members', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('created_by')->constrained('users')->onDelete('cascade');
+            $table->string('member_code')->unique();
+            $table->string('name');
+            $table->enum('gender', ['male', 'female']);
+            $table->date('birth_date');
+            $table->text('address');
+            $table->string('phone');
+            $table->string('email');
+            $table->date('join_date');
+            $table->enum('status', ['active', 'inactive'])->default('active');
+            $table->string('image')->nullable();
+            $table->timestamps();
+        });
     }
 
     /**
