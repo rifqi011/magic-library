4a2e8fdf8c7486ddfc5107326265f7c431b1d834
Rifqi Banu Safingi <rifqibanusafingi11@gmail.com>
2025-10-26 20:47:06 +0700

    feat: implement Category resource with create, edit, and list pages

---
+++ app/Filament/Admin/Resources/CategoryResource.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Filament\Admin\Resources;
+
+use Filament\Forms;
+use Filament\Tables;
+use App\Models\Category;
+use Filament\Forms\Form;
+use Filament\Tables\Table;
+use Illuminate\Support\Str;
+use Filament\Resources\Resource;
+use Filament\Tables\Columns\IconColumn;
+use Filament\Tables\Columns\TextColumn;
+use Illuminate\Database\Eloquent\Builder;
+use Filament\Tables\Filters\TernaryFilter;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use App\Filament\Admin\Resources\CategoryResource\Pages;
+use App\Filament\Admin\Resources\CategoryResource\RelationManagers;
+
+class CategoryResource extends Resource
+{
+    protected static ?string $model = Category::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-tag';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('name')
+                    ->label('Name')
+                    ->required()
+                    ->maxLength(255)
+                    ->live(onBlur: true)
+                    ->afterStateUpdated(function (string $state, Forms\Set $set) {
+                        $set('slug', Str::slug($state));
+                    }),
+
+                Forms\Components\TextInput::make('slug')
+                    ->label('Slug')
+                    ->readOnly()
+                    ->maxLength(255)
+                    ->unique(ignoreRecord: true)
+                    ->dehydrated(),
+
+                Forms\Components\TextInput::make('description')
+                    ->required()
+                    ->maxLength(255),
+
+                Forms\Components\Select::make('status')
+                    ->options([
+                        'active' => 'Active',
+                        'inactive' => 'Inactive',
+                    ])
+                    ->required()
+                    ->default('active')
+                    ->native(false)
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('name')
+                    ->searchable()
+                    ->sortable(),
+
+                TextColumn::make('description')
+                    ->limit(30),
+
+                IconColumn::make('status')
+                    ->boolean()
+                    ->getStateUsing(fn(Category $record): bool => $record->status == 'active')
+                    ->sortable()
+                    ->label('Active')
+            ])
+            ->filters([
+                TernaryFilter::make('status')
+                    ->trueLabel('Active')
+                    ->falseLabel('Inactive')
+                    ->native()
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\ViewAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListCategories::route('/'),
+            'create' => Pages\CreateCategory::route('/create'),
+            'edit' => Pages\EditCategory::route('/{record}/edit'),
+        ];
+    }
+}
