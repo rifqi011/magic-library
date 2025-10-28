e70f625c4e6df14ba3209e0ea645a5b2077eba61
Rifqi Banu Safingi <rifqibanusafingi11@gmail.com>
2025-10-28 10:05:07 +0700

    feat: create genre resource

---
+++ app/Filament/Admin/Resources/GenreResource.php
@@ -0,0 +1,122 @@
+<?php
+
+namespace App\Filament\Admin\Resources;
+
+use Filament\Forms;
+use Filament\Tables;
+use App\Models\Genre;
+use Filament\Forms\Form;
+use Filament\Tables\Table;
+use Illuminate\Support\Str;
+use Filament\Resources\Resource;
+use Filament\Forms\Components\Select;
+use Filament\Tables\Actions\EditAction;
+use Filament\Tables\Actions\ViewAction;
+use Filament\Tables\Columns\IconColumn;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Forms\Components\TextInput;
+use Filament\Tables\Actions\DeleteAction;
+use Illuminate\Database\Eloquent\Builder;
+use Filament\Tables\Filters\TernaryFilter;
+use Filament\Tables\Actions\BulkActionGroup;
+use Filament\Tables\Actions\DeleteBulkAction;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use App\Filament\Admin\Resources\GenreResource\Pages;
+use App\Filament\Admin\Resources\GenreResource\Pages\EditGenre;
+use App\Filament\Admin\Resources\GenreResource\Pages\ListGenres;
+use App\Filament\Admin\Resources\GenreResource\RelationManagers;
+use App\Filament\Admin\Resources\GenreResource\Pages\CreateGenre;
+
+class GenreResource extends Resource
+{
+    protected static ?string $model = Genre::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+            Forms\Components\TextInput::make('name')
+                ->label('Name')
+                ->required()
+                ->maxLength(255)
+                ->live(onBlur: true)
+                ->afterStateUpdated(function (string $state, Forms\Set $set) {
+                    $set('slug', Str::slug($state));
+                }),
+
+            Forms\Components\TextInput::make('slug')
+                ->label('Slug')
+                ->readOnly()
+                ->maxLength(255)
+                ->unique(ignoreRecord: true)
+                ->dehydrated(),
+
+            Forms\Components\TextInput::make('description')
+                ->required()
+                ->maxLength(255),
+
+            Forms\Components\Select::make('status')
+                ->options([
+                    'active' => 'Active',
+                    'inactive' => 'Inactive',
+                ])
+                ->required()
+                ->default('active')
+                ->native(false)
+
+        ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('name')
+                    ->searchable()
+                    ->sortable(),
+
+                TextColumn::make('description')
+                    ->limit(30),
+
+                IconColumn::make('status')
+                    ->boolean()
+                    ->getStateUsing(fn(Genre $record): bool => $record->status == 'active')
+                    ->sortable()
+                    ->label('Active')
+            ])
+            ->filters([
+                TernaryFilter::make('status')
+                    ->trueLabel('Active')
+                    ->falseLabel('Inactive')
+                    ->native()
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\ViewAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListGenres::route('/'),
+            'create' => Pages\CreateGenre::route('/create'),
+            'edit' => Pages\EditGenre::route('/{record}/edit'),
+        ];
+    }
+}
---
+++ app/Filament/Admin/Resources/GenreResource/Pages/CreateGenre.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Filament\Admin\Resources\GenreResource\Pages;
+
+use App\Filament\Admin\Resources\GenreResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateGenre extends CreateRecord
+{
+    protected static string $resource = GenreResource::class;
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
+}
